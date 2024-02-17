8b20254500a810e4c36a04434b36b1a7113ca362
Jacob Vosmaer <contact@jacobvosmaer.nl>
2024-02-17 10:57:52 +0100

    Section 2.2

--- main.c
+++ main.c
@@ -1,1 +1,15 @@
-int main (void){}
+#include <stdio.h>
+
+int main(void) {
+  int imagewidth = 256, imageheight = 256, i, j;
+
+  printf("P3\n%d %d\n255\n", imagewidth, imageheight);
+  for (j = 0; j < imageheight; j++) {
+    for (i = 0; i < imagewidth; i++) {
+      double r = (double)i / (imagewidth - 1),
+             g = (double)j / (imageheight - 1), b = 0;
+      int ir = 255.999 * r, ig = 255.999 * g, ib = 255.999 * b;
+      printf("%d %d %d\n", ir, ig, ib);
+    }
+  }
+}
