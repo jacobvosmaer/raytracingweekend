5ba2f6172237bf7f405a0261b9012b4afc0b88a3
Jacob Vosmaer <contact@jacobvosmaer.nl>
2024-02-17 11:19:21 +0100

    Chapter 3

--- main.c
+++ main.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
 
+typedef struct {
+  double x, y, z;
+} vec3;
+
+vec3 v3(double x,double y,double z){
+vec3 v;
+v.x=x;v.y=y;v.z=z;return v;}
+
+void writecolor(FILE *out, vec3 color) {
+  double s = 255.999;
+  fprintf(out, "%d %d %d\n", (int)(s * color.x), (int)(s * color.y),
+          (int)(s * color.z));
+}
+
 int main(void) {
   int imagewidth = 256, imageheight = 256, i, j;
 
   printf("P3\n%d %d\n255\n", imagewidth, imageheight);
   for (j = 0; j < imageheight; j++) {
     for (i = 0; i < imagewidth; i++) {
-      double r = (double)i / (imagewidth - 1),
-             g = (double)j / (imageheight - 1), b = 0;
-      int ir = 255.999 * r, ig = 255.999 * g, ib = 255.999 * b;
-      printf("%d %d %d\n", ir, ig, ib);
+      vec3 pixelcolor = v3((double)i / (imagewidth - 1),
+                         (double)j / (imageheight - 1), 0);
+      writecolor(stdout, pixelcolor);
     }
   }
 }
