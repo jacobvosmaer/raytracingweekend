e4f78859eec12cafe9b2d77f41e5ecdadeb6e714
Jacob Vosmaer <contact@jacobvosmaer.nl>
2024-03-06 00:11:45 +0100

    Add tail call recursion

--- main.c
+++ main.c
@@ -300,25 +300,30 @@ int scatter(material mat, ray in, hitrecord *rec, vec3 *attenuation,
  * once per pixel we still get the effect that dielectrics both reflect _and_
  * refract. */
 vec3 raycolor(ray r, int depth, spherelist *world) {
-  vec3 black = {0};
-  hitrecord rec;
+  vec3 black = {0}, attenuation = v3(1, 1, 1);
 
-  if (depth <= 0)
-    return black;
-
-  if (spherelisthit(world, r, interval(0.001, INFINITY), &rec)) {
-    /* ray has hit an object */
-    ray scattered;
-    vec3 attenuation;
-    if (scatter(rec.mat, r, &rec, &attenuation, &scattered))
-      return v3mul(attenuation, raycolor(scattered, depth - 1, world));
-    return black;
-  } else {
-    /* ray has hit the sky */
-    vec3 dir = v3unit(r.dir);
-    scalar a = 0.5 * (v3y(dir) + 1.0);
-    return v3add(v3scale(v3(1, 1, 1), 1.0 - a), v3scale(v3(0.5, 0.7, 1), a));
+  while (depth > 0) {
+    hitrecord rec;
+    if (spherelisthit(world, r, interval(0.001, INFINITY), &rec)) {
+      /* ray has hit an object */
+      ray scattered;
+      vec3 newattenuation;
+      if (scatter(rec.mat, r, &rec, &newattenuation, &scattered)) {
+        r = scattered;
+        attenuation = v3mul(attenuation, newattenuation);
+        depth--;
+        continue;
+      }
+      return black;
+    } else {
+      /* ray has hit the sky */
+      vec3 dir = v3unit(r.dir);
+      scalar a = 0.5 * (v3y(dir) + 1.0);
+      return v3mul(attenuation, v3add(v3scale(v3(1, 1, 1), 1.0 - a),
+                                      v3scale(v3(0.5, 0.7, 1), a)));
+    }
   }
+  return black;
 }
 
 typedef struct {
