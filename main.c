d2bf34213e2e7b797df91cf32d6b553d8004cb0d
Jacob Vosmaer <contact@jacobvosmaer.nl>
2024-03-06 13:05:15 +0100

    Also use multiply-subtract

--- main.c
+++ main.c
@@ -190,10 +190,12 @@ void hitrecordsetnormal(hitrecord *rec, ray r, vec3 outwardnormal) {
 int spherehit4(struct sphere4 sp, ray r, struct interval t, int maxi,
                hitrecord *rec) {
   vec3x4 rdir = v3x4load(r.dir), oc = v3x4sub(v3x4load(r.orig), sp.center);
-  scalar4 a = v3x4dot(rdir, rdir),
-          halfbneg = s4sub(s4load(0), v3x4dot(oc, rdir)),
-          c = s4sub(v3x4dot(oc, oc), s4mul(sp.radius, sp.radius)),
-          discriminant = s4sub(s4mul(halfbneg, halfbneg), s4mul(a, c));
+  scalar4 zero4 = {0}, a = v3x4dot(rdir, rdir);
+  /* halfbneg = -dot(oc, rdir) */
+  scalar4 halfbneg = s4mulsub(
+      s4mulsub(s4mulsub(zero4, oc.x, rdir.x), oc.y, rdir.y), oc.z, rdir.z);
+  scalar4 c = s4mulsub(v3x4dot(oc, oc), sp.radius, sp.radius),
+          discriminant = s4mulsub(s4mul(halfbneg, halfbneg), a, c);
   scalar4 sqrtd, rootmin, rootmax;
   int i, hit;
 
--- scalar.h
+++ scalar.h
@@ -29,6 +29,7 @@ scalar4 s4loadat(scalar4 a, scalar x, int i);
 scalar4 s4abs(scalar4 a);
 scalar4 s4sqrt(scalar4 a);
 scalar4 s4mulacc(scalar4 a, scalar4 b, scalar4 c);
+scalar4 s4mulsub(scalar4 a, scalar4 b, scalar4 c);
 
 scalar s4max(scalar4 a);
 scalar s4get(scalar4 a, int i);
